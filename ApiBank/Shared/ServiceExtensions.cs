cd2c4d2b999d1db75d8dca3bf1ed4de8a0cc28f3
Leonardo Vazquez <92757623+Yashiuwu@users.noreply.github.com>
2022-10-28 21:49:08 -0500

    Add files via upload

---
+++ ApiBank/Shared/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Aplication.Interface;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared
+{
+    public static class ServiceExtensions
+    {
+        public static void AddShareInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDateTimeService, IDateTimeService>();
+        }
+    }
+}
---
+++ ApiBank/Shared/Services/DateTimeService.cs
@@ -0,0 +1,10 @@
+﻿using Aplication.Interface;
+using System;
+
+namespace Shared.Services
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime NowUtc => DateTime.UtcNow;
+    }
+}
